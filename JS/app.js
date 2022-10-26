bab1755a088f3c0b139d8ebdf76a682827e0ec13
MattZKruspe <matthewmarquez2003@gmail.com>
2022-10-25 23:08:27 -0500

    segundo Commit

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+import { navegacion, showModal, loadCharacterInfo, loadData, showCharacters, urlBase } from './eventos.js';
+
+document.querySelector('#respuesta').addEventListener('click', showModal);
+
+loadData(urlBase);
+
+document.querySelector('.botones').addEventListener('click', navegacion);
